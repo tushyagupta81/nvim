8898f4e77c8e6a46c82579ef8bec41baed29c4d6
Tushya Gupta <tushyagupta81@gmail.com>
2025-01-09 09:12:46 +0530

    changes to blink.cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -37,8 +37,10 @@ return {
 				nerd_font_variant = "mono",
 			},
 
+      snippets = {preset="luasnip"},
+
 			sources = {
-				default = { "lsp", "lazydev", "path", "snippets", "buffer", "luasnip" },
+				default = { "lsp", "lazydev", "path", "snippets", "buffer"},
 				providers = {
 					lsp = {
 						name = "lsp",
@@ -53,46 +55,46 @@ return {
 						score_offset = 100, -- show at a higher priority than lsp
 						fallbacks = { "lsp" },
 					},
-					luasnip = {
-						name = "luasnip",
-						enabled = true,
-						module = "blink.cmp.sources.luasnip",
-						should_show_items = function()
-							local col = vim.api.nvim_win_get_cursor(0)[2]
-							local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
-							-- NOTE: remember that `trigger_text` is modified at the top of the file
-							return before_cursor:match(trigger_text .. "%w*$") ~= nil
-						end,
-						-- After accepting the completion, delete the trigger_text characters
-						-- from the final inserted text
-						transform_items = function(ctx, items)
-							-- WARNING: Explicitly referencing ctx otherwise I was getting an "unused" warning
-							local _ = ctx
-							local col = vim.api.nvim_win_get_cursor(0)[2]
-							local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
-							local trigger_pos = before_cursor:find(trigger_text .. "[^" .. trigger_text .. "]*$")
-							if trigger_pos then
-								for _, item in ipairs(items) do
-									item.textEdit = {
-										newText = item.insertText or item.label,
-										range = {
-											start = { line = vim.fn.line(".") - 1, character = trigger_pos - 1 },
-											["end"] = { line = vim.fn.line(".") - 1, character = col },
-										},
-									}
-								end
-							end
-							-- NOTE: After the transformation, I have to reload the luasnip source
-							-- Otherwise really crazy shit happens and I spent way too much time
-							-- figurig this out
-							vim.schedule(function()
-								require("blink.cmp").reload("luasnip")
-							end)
-							return items
-						end,
-						fallbacks = { "snippets" },
-						score_offset = 85,
-					},
+					-- luasnip = {
+					-- 	name = "luasnip",
+					-- 	enabled = true,
+					-- 	module = "blink.cmp.sources.luasnip",
+					-- 	should_show_items = function()
+					-- 		local col = vim.api.nvim_win_get_cursor(0)[2]
+					-- 		local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
+					-- 		-- NOTE: remember that `trigger_text` is modified at the top of the file
+					-- 		return before_cursor:match(trigger_text .. "%w*$") ~= nil
+					-- 	end,
+					-- 	-- After accepting the completion, delete the trigger_text characters
+					-- 	-- from the final inserted text
+					-- 	transform_items = function(ctx, items)
+					-- 		-- WARNING: Explicitly referencing ctx otherwise I was getting an "unused" warning
+					-- 		local _ = ctx
+					-- 		local col = vim.api.nvim_win_get_cursor(0)[2]
+					-- 		local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
+					-- 		local trigger_pos = before_cursor:find(trigger_text .. "[^" .. trigger_text .. "]*$")
+					-- 		if trigger_pos then
+					-- 			for _, item in ipairs(items) do
+					-- 				item.textEdit = {
+					-- 					newText = item.insertText or item.label,
+					-- 					range = {
+					-- 						start = { line = vim.fn.line(".") - 1, character = trigger_pos - 1 },
+					-- 						["end"] = { line = vim.fn.line(".") - 1, character = col },
+					-- 					},
+					-- 				}
+					-- 			end
+					-- 		end
+					-- 		-- NOTE: After the transformation, I have to reload the luasnip source
+					-- 		-- Otherwise really crazy shit happens and I spent way too much time
+					-- 		-- figurig this out
+					-- 		vim.schedule(function()
+					-- 			require("blink.cmp").reload("luasnip")
+					-- 		end)
+					-- 		return items
+					-- 	end,
+					-- 	fallbacks = { "snippets" },
+					-- 	score_offset = 85,
+					-- },
 					path = {
 						name = "Path",
 						module = "blink.cmp.sources.path",
