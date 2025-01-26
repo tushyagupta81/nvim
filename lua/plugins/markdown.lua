7d8263f2d4aab26a514d7a535caf48b94b0deb83
Tushya Gupta <tushyagupta81@gmail.com>
2025-01-26 19:40:39 +0530

    moved to markdown previewer

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,6 +1,15 @@
 return {
-	"OXY2DEV/markview.nvim",
-	ft = { "md", "markdown" },
-	event = { "BufReadPre", "BufNewFile" },
-	opts = {},
+	-- {
+	-- 	"OXY2DEV/markview.nvim",
+	-- 	event = { "BufReadPre", "BufNewFile" },
+	-- 	opts = {},
+	-- },
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
 }
