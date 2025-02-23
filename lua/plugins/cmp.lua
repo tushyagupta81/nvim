1a2effb8a22a133276e39cfc59978062f695c37e
Tushya Gupta <tushyagupta81@gmail.com>
2025-02-23 10:23:42 +0530

    lazylock changes and blink.cmp new breaking changes

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -124,15 +124,36 @@ return {
 						async = true,
 					},
 				},
-        cmdline = {},
-				-- cmdline = function()
-				-- 	local type = vim.fn.getcmdtype()
-				-- 	-- Search forward and backward
-				-- 	if type == "/" or type == "?" then
-				-- 		return { "buffer" }
-				-- 	end
-				-- 	return {}
-				-- end,
+			},
+
+			cmdline = {
+				enabled = true,
+				keymap = nil, -- Inherits from top level `keymap` config when not set
+				---@diagnostic disable-next-line: assign-type-mismatch
+				sources = function()
+					local type = vim.fn.getcmdtype()
+					-- Search forward and backward
+					if type == "/" or type == "?" then
+						return { "buffer" }
+					end
+					-- Commands
+					if type == ":" or type == "@" then
+						return { "cmdline" }
+					end
+					return {}
+				end,
+				completion = {
+					trigger = {
+						show_on_blocked_trigger_characters = {},
+						show_on_x_blocked_trigger_characters = nil, -- Inherits from top level `completion.trigger.show_on_blocked_trigger_characters` config when not set
+					},
+					menu = {
+						auto_show = nil, -- Inherits from top level `completion.menu.auto_show` config when not set
+						draw = {
+							columns = { { "label", "label_description", gap = 1 } },
+						},
+					},
+				},
 			},
 
 			completion = {
