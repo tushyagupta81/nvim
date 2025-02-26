d8535f29f314813e7864610d419f82f5afb6f971
Tushya Gupta <tushyagupta81@gmail.com>
2025-02-26 20:55:09 +0530

    trying out flash again

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -78,7 +78,7 @@ return {
 							local col = vim.api.nvim_win_get_cursor(0)[2]
 							local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
 							-- NOTE: remember that `trigger_text` is modified at the top of the file
-							return before_cursor:match(trigger_text .. "%w*$") ~= nil
+							return before_cursor:match(trigger_text .. "%w*$") ~= nil or true
 						end,
 						-- After accepting the completion, delete the trigger_text characters
 						-- from the final inserted text
