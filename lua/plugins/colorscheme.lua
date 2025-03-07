bc3ac5e6e8cf0ad56d7efb2fe1e1ef8ed8004f6b
Tushya Gupta <tushyagupta81@gmail.com>
2025-03-07 17:19:02 +0530

    Blink Transparent background changes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,6 +3,7 @@ function ColorMyPencils(color)
 	color = color or "cyberdream"
 	vim.cmd.colorscheme(color)
 
+  vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 	vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ffbd5e" })
