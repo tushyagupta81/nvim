f08190fd894c665288cb3ced75425830ab909a43
Tushya Gupta <tushyagupta81@gmail.com>
2025-04-20 13:53:32 +0530

    markdown one more plugin + lazylock changes

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,12 +1,24 @@
 return {
-	"iamcco/markdown-preview.nvim",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	ft = { "markdown" },
-	build = function()
-		-- vim.fn["mkdp#util#install"]()
-    vim.cmd([[call mkdp#util#install()]])
-	end,
-  config = function ()
-    vim.keymap.set("n","<leader>pre","<cmd>MarkdownPreview<CR>")
-  end
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			-- vim.fn["mkdp#util#install"]()
+			vim.cmd([[call mkdp#util#install()]])
+		end,
+		config = function()
+			vim.keymap.set("n", "<leader>pre", "<cmd>MarkdownPreview<CR>")
+		end,
+	},
+	{
+		"OXY2DEV/markview.nvim",
+		lazy = false,
+
+		-- For blink.cmp's completion
+		-- source
+		-- dependencies = {
+		--     "saghen/blink.cmp"
+		-- },
+	},
 }
