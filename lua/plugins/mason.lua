a5dfd74e084392f30bd1355de245dbf4632db36a
Tushya Gupta <tushyagupta81@gmail.com>
2025-05-12 15:02:37 +0530

    mason-nvim-dap to add adapterse to nvim-dap

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,51 +1,64 @@
 ---@diagnostic disable: missing-fields
 return {
-	"williamboman/mason.nvim",
-	event = "VeryLazy",
-	dependencies = {
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		require("mason").setup({
-			-- PATH = "append",
-			ui = {
-				border = "single",
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
+	{
+		"williamboman/mason.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+		},
+		config = function()
+			require("mason").setup({
+				-- PATH = "append",
+				ui = {
+					border = "single",
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
 				},
-			},
-		})
-		require("mason-tool-installer").setup({
-			ensure_installed = {
-				-- Formatters and Linters
-				"prettier", -- js, ts & more formatter
-				"stylua", -- lua formatter
-				"ruff", -- python linter & formatter
-				"biome", -- js & ts formatter
-				-- "eslint_d", -- js formatter
-				"clang-format", -- cpp & c formatter
-				"marksman", -- markdown lsp
-				-- "markdownlint-cli2", -- markdown linter
+			})
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					-- Formatters and Linters
+					"prettier", -- js, ts & more formatter
+					"stylua", -- lua formatter
+					"ruff", -- python linter & formatter
+					"biome", -- js & ts formatter
+					-- "eslint_d", -- js formatter
+					"clang-format", -- cpp & c formatter
+					"marksman", -- markdown lsp
+					-- "markdownlint-cli2", -- markdown linter
 
-				-- LSP's
-				"lua-language-server", -- lua
-				"rust-analyzer", -- rust
-				"typescript-language-server", -- typescript
-				"emmet-language-server", -- emmet
-				-- "emmet_ls", -- emmet
-				-- "eslint", -- javascript
-				"basedpyright", -- python
-				"bash-language-server", -- bash
-				"tailwindcss-language-server", -- tailwind
-				"zls", -- zig
-				"clangd", -- cpp
+					-- LSP's
+					"lua-language-server", -- lua
+					"rust-analyzer", -- rust
+					"typescript-language-server", -- typescript
+					"emmet-language-server", -- emmet
+					-- "emmet_ls", -- emmet
+					-- "eslint", -- javascript
+					"basedpyright", -- python
+					"bash-language-server", -- bash
+					"tailwindcss-language-server", -- tailwind
+					"zls", -- zig
+					"clangd", -- cpp
 
-				-- Debug adapters
-				"codelldb", -- c c++ rust zig
-				"debugpy", -- python
-			},
-		})
-	end,
+					-- Debug adapters
+					"codelldb", -- c c++ rust zig
+					"debugpy", -- python
+				},
+			})
+		end,
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"mfussenegger/nvim-dap",
+		},
+		opts = {
+			handlers = {},
+		},
+	},
 }
