66d6e206bf9d0d31e3b04ac9f05bb9635efcb130
Tushya Gupta <tushyagupta81@gmail.com>
2025-08-14 14:03:09 +0530

    fixing cmp priority list

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -41,69 +41,23 @@ return {
 
 		sources = {
 			default = { "lsp", "snippets", "path", "buffer" },
-			providers = {
-				lsp = {
-					score_offset = 90,
-				},
-				snippets = {
-					name = "snippets",
-					enabled = true,
-					max_items = 15,
-					-- min_keyword_length = 2,
-					module = "blink.cmp.sources.snippets",
-					score_offset = 85, -- the higher the number, the higher the priority
-					-- Only show snippets if I type the trigger_text characters, so
-					-- to expand the "bash" snippet, if the trigger_text is ";" I have to
-					should_show_items = function()
-						local col = vim.api.nvim_win_get_cursor(0)[2]
-						local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
-						-- NOTE: remember that `trigger_text` is modified at the top of the file
-						return before_cursor:match(trigger_text .. "%w*$") ~= nil or true
-					end,
-					-- After accepting the completion, delete the trigger_text characters
-					-- from the final inserted text
-					transform_items = function(_, items)
-						local col = vim.api.nvim_win_get_cursor(0)[2]
-						local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
-						local trigger_pos = before_cursor:find(trigger_text .. "[^" .. trigger_text .. "]*$")
-						if trigger_pos then
-							for _, item in ipairs(items) do
-								item.textEdit = {
-									newText = item.insertText or item.label,
-									range = {
-										start = { line = vim.fn.line(".") - 1, character = trigger_pos - 1 },
-										["end"] = { line = vim.fn.line(".") - 1, character = col },
-									},
-								}
-							end
-						end
-						-- NOTE: After the transformation, I have to reload the luasnip source
-						-- Otherwise really crazy shit happens and I spent way too much time
-						-- figurig this out
-						vim.schedule(function()
-							require("blink.cmp").reload("snippets")
-						end)
-						return items
-					end,
-
-					fallbacks = function()
-						local col = vim.api.nvim_win_get_cursor(0)[2]
-						local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
-						-- NOTE: remember that `trigger_text` is modified at the top of the file
-						if before_cursor:match(trigger_text .. "%w*$") ~= nil then
-							return { "lsp" }
-						else
-							return {}
-						end
-					end,
-				},
-				-- codeium = {
-				--       name = "Codeium",
-				--       module = "codeium.blink",
-				-- 	score_offset = 80,
-				--       async = true,
-				--     },
-			},
+			-- providers = {
+			-- 	lsp = {
+			-- 		score_offset = 90,
+			-- 	},
+			--      path = {
+			--        score_offset = 85
+			--      },
+			-- 	snippets = {
+			-- 		score_offset = 80,
+			-- 	},
+			-- 	-- codeium = {
+			-- 	--       name = "Codeium",
+			-- 	--       module = "codeium.blink",
+			-- 	-- 	score_offset = 80,
+			-- 	--       async = true,
+			-- 	--     },
+			-- },
 		},
 
 		cmdline = {
