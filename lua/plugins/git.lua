478fb7e542d64200362a6720fe74a2b965b21204
Tushya Gupta <tushyagupta81@gmail.com>
2025-12-21 17:55:14 +0530

    Added config for gitdiff

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -21,12 +21,60 @@ return {
 		"tpope/vim-fugitive",
 		event = "VeryLazy",
 		dependencies = {
-      "sindrets/diffview.nvim",
-      event = "VeryLazy",
-    },
+			"sindrets/diffview.nvim",
+			event = "VeryLazy",
+		},
 		config = function()
 			vim.keymap.set("n", "<leader>dvo", ":DiffviewOpen<CR>")
 			vim.keymap.set("n", "<leader>dvc", ":DiffviewClose<CR>")
+			require("diffview").setup({
+				diff_binaries = false, -- Show diffs for binaries
+				enhanced_diff_hl = false, -- See |diffview-config-enhanced_diff_hl|
+				git_cmd = { "git" }, -- The git executable followed by default args.
+				hg_cmd = { "hg" }, -- The hg executable followed by default args.
+				use_icons = true, -- Requires nvim-web-devicons
+				show_help_hints = true, -- Show hints for how to open the help panel
+				watch_index = true, -- Update views and index buffers when the git index changes.
+				icons = { -- Only applies when use_icons is true.
+					folder_closed = "",
+					folder_open = "",
+				},
+				signs = {
+					fold_closed = "",
+					fold_open = "",
+					done = "✓",
+				},
+				view = {
+					-- Configure the layout and behavior of different types of views.
+					-- Available layouts:
+					--  'diff1_plain'
+					--    |'diff2_horizontal'
+					--    |'diff2_vertical'
+					--    |'diff3_horizontal'
+					--    |'diff3_vertical'
+					--    |'diff3_mixed'
+					--    |'diff4_mixed'
+					-- For more info, see |diffview-config-view.x.layout|.
+					default = {
+						-- Config for changed files, and staged files in diff views.
+						layout = "diff2_horizontal",
+						disable_diagnostics = false, -- Temporarily disable diagnostics for diff buffers while in the view.
+						winbar_info = false, -- See |diffview-config-view.x.winbar_info|
+					},
+					merge_tool = {
+						-- Config for conflicted files in diff views during a merge or rebase.
+						layout = "diff3_mixed",
+						disable_diagnostics = true, -- Temporarily disable diagnostics for diff buffers while in the view.
+						winbar_info = true, -- See |diffview-config-view.x.winbar_info|
+					},
+					file_history = {
+						-- Config for changed files in file history views.
+						layout = "diff2_horizontal",
+						disable_diagnostics = false, -- Temporarily disable diagnostics for diff buffers while in the view.
+						winbar_info = false, -- See |diffview-config-view.x.winbar_info|
+					},
+				},
+			})
 		end,
 	},
 }
