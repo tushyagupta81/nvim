c1b72801c4fb5dc89c4885b81f548ea319fd9381
Tushya Gupta <tushyagupta81@gmail.com>
2026-05-23 19:02:41 +0530

    nvim-pack-lock.json

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,7 +1,13 @@
-vim.api.nvim_create_user_command("SyncPack", function ()
+vim.api.nvim_create_user_command("PackSync", function ()
   vim.pack.update(nil, {
     target = 'lockfile',
   })
 end, {
   desc = "Sync plugins to lockfile",
+})
+
+vim.api.nvim_create_user_command("PackUpdate", function ()
+  vim.pack.update()
+end, {
+  desc = "Update all plugins",
 })
