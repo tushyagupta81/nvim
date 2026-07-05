c449aa23aea225a8b25606fd4f5548a7ee7808fe
Tushya Gupta <tushyagupta81@gmail.com>
2026-07-05 19:09:29 +0530

    added nvim-dap config and adapater for cpp

--- plugin/dap.lua
+++ plugin/dap.lua
@@ -95,5 +95,38 @@ vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
 				vim.keymap.set("n", "q", "<C-w>q", { buffer = args.buf })
 			end,
 		})
+		dap.configurations.cpp = {
+			{
+				name = "Launch",
+				type = "lldb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopOnEntry = false,
+				args = {},
+
+				-- 💀
+				-- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+				--
+				--    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+				--
+				-- Otherwise you might get the following error:
+				--
+				--    Error on launch: Failed to attach to the target process
+				--
+				-- But you should be aware of the implications:
+				-- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+				-- runInTerminal = false,
+			},
+		}
+		local lldb = vim.fn.exepath("codelldb")
+
+		dap.adapters.lldb = {
+			type = "executable",
+			command = lldb,
+			name = "lldb",
+		}
 	end,
 })
